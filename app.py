746075205290fb8b38a857cca8cf82b5f66af7df
AfrizalSeptiansyah <mafrizalseptiansyah@gmail.com>
2021-09-01 20:02:06 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+import pandas as pd 
+from jcopml.utils import load_model
+
+app = Flask(__name__)
+model = load_model('model/churn_xgb.pkl')
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        data = request.form.to_dict()
+        X_test = pd.DataFrame([data])
+        X_test.number_customer_service_calls = X_test.number_customer_service_calls.map({'no': 0, 'yes': 1})
+        hasil = model.predict(X_test)
+        return render_template('hasil.html', FinalData=hasil)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000', debug=True)
